0dd1bcb531770dfb331e3f916350d9160f12c6fc
Smey <roemreaksmey7@gmail.com>
2024-11-04 09:06:14 +0700

    respone pricing page

--- apps/client-app/app/price/components/molecules/PricingTable.tsx
+++ apps/client-app/app/price/components/molecules/PricingTable.tsx
@@ -16,24 +16,27 @@ export const PricingTable = () => {
   return (
     <div className="max-w-5xl p-4 mx-auto overflow-hidde">
       {/* Header */}
-      <div className="grid grid-cols-[2fr_1fr_1fr] border-b border-gray-300">
-        <div className="flex w-full justify-end p-6 text-center">
-          <div className="bg-[#E6EDFF] w-[35%] p-2 rounded-md">
-            <h3 className="text-[16px] text-blue-600">Free Trial 14 Day</h3>
-            <p className="text-xl">0$</p>
+      <div className="flex flex-row border-b border-gray-300">
+        <div className="p-1 md:p-6 w-[50%] text-center flex justify-end">
+          <div className="bg-[#E6EDFF] w-[50%] px-1 py-2 rounded-md">
+            <h3 className="text-[14px] md:text-[16px] text-blue-600">
+              Free Trial 14 Day
+            </h3>
+            <p className="text-[14px] md:text-[16px]">0$</p>
           </div>
         </div>
-        <div className="flex justify-center w-full p-6 text-center text-white border-l border-r border-t border-gray-300 rounded-t-lg">
-          <div className="bg-[#443DFF] w-[80%] px-1 py-2 rounded-md">
-            <h3 className="text-[16px]">Monthly Plan</h3>
-            <p className="text-xl">10$/Month</p>
+        <div className="flex w-[25%] justify-center p-1 md:p-6 text-center text-white border-l border-r border-t border-gray-300 rounded-t-lg">
+          <div className="bg-[#443DFF] w-full px-1 py-2 rounded-md">
+            <h3 className="text-[14px] md:text-[16px]">Monthly Plan</h3>
+            <p className="text-[14px] md:text-[16px]">10$/Month</p>
           </div>
         </div>
-
-        <div className="p-6 text-center w-full">
-          <div className="bg-[#E6EDFF] w-[80%] px-1 py-2 rounded-md">
-            <h3 className="text-[16px] text-blue-600">Yearly Plan</h3>
-            <p className="text-xl">100$/Year</p>
+        <div className="p-1 md:p-6 w-[25%] text-center">
+          <div className="bg-[#E6EDFF] w-full px-1 py-2 rounded-md">
+            <h3 className="text-[14px] md:text-[16px] text-blue-600">
+              Yearly Plan
+            </h3>
+            <p className="text-[14px] md:text-[16px]">100$/Year</p>
           </div>
         </div>
       </div>
@@ -43,18 +46,18 @@ export const PricingTable = () => {
         {features.map((feature, index) => (
           <div
             key={index}
-            className="grid grid-cols-[2fr_1fr_1fr] divide-x divide-gray-300"
+            className="grid grid-cols-[2fr_1fr_1fr] divide-x divide-gray-300 items-center justify-center"
           >
-            <div className="flex items-center justify-between p-4 pr-[20%]">
+            <div className="flex items-center justify-between space-x-1 pr-[10%] md:pr-[20%]">
               <p className="text-gray-600">{feature}</p>
-              {/* <AiOutlineClose className="text-blue-500 mt-2" /> */}
-              <FaCheck className="text-blue-500 mt-2" />
+              {/* <AiOutlineClose className="text-blue-500" /> */}
+              <FaCheck className="text-blue-500" />
             </div>
             <div className="flex p-4 justify-center">
-              <FaCheck className="text-blue-500 mt-2" />
+              <FaCheck className="text-blue-500" />
             </div>
             <div className="flex p-4 justify-center">
-              <FaCheck className="text-blue-500 mt-2" />
+              <FaCheck className="text-blue-500" />
             </div>
           </div>
         ))}
