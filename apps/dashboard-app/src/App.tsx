d3a7786acbd0b03ee1f2c9fa7d04b07228cb5196
hong <etkimhong@gmail.com>
2024-11-04 11:30:54 +0700

    first

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -1,19 +1,25 @@
 import Layout from "@/src/components/organisms/layout/MainLayout";
 import "./styles.css";
-import { BrowserRouter as Router, Route, BrowserRouter, Routes } from "react-router-dom";
+import Button from "./components/atoms/Button";
+import { InputTest } from "./components/atoms/Input";
+import {
+  BrowserRouter as Router,
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 import Visualize from "./components/pages/Visualize";
 import Dataset from "./components/pages/Dataset";
 import Project from "./components/pages/Project";
 import Helps from "./components/pages/Helps";
-import AccountSettings from "./components/templates/AccountSetting";
+import AccountSettings from "./components/Templates/AccountSetting";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Project />} />
-          <Route path="/resule-cleanig" element={<ShowResuleCleaning />} />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Project />} />
           <Route path="/visualize" element={<Visualize />} />
           <Route path="/dataset" element={<Dataset />} />
           <Route path="/helps" element={<Helps />} />
