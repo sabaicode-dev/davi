f09f9d0d94421c9df0886b985b2d0c2505dbbc5f
sirblue <sophearum14@gmail.com>
2024-11-05 10:59:07 +0700

    added laoding & icon nav sm screen

--- apps/client-app/app/loading.tsx
+++ apps/client-app/app/loading.tsx
@@ -1,13 +1,13 @@
-import { Loader2 } from "lucide-react";
+import { Ellipsis } from "lucide-react";
 
 export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <div className="text-center">
-        <Loader2 className="h-16 w-16 animate-spin text-primary mx-auto text-blue-700" />
-        <h2 className="mt-4 text-xl font-semibold text-foreground text-blue-700">
+        <Ellipsis  className="h-20 w-20 animate-ping text-primary mx-auto text-blue-700" />
+        {/* <h2 className="mt-4 text-xl font-semibold text-foreground text-blue-700">
           Loading...
-        </h2>
+        </h2> */}
       </div>
     </div>
   );
