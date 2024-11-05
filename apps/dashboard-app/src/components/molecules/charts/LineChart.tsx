db6ad81a8b0103d4e5b69f0c46ac6f4141413d23
easycode002 <easycode002@gmail.com>
2024-11-05 14:24:44 +0700

    Added: Visualize page

--- apps/dashboard-app/src/components/molecules/charts/LineChart.tsx
+++ apps/dashboard-app/src/components/molecules/charts/LineChart.tsx
@@ -68,7 +68,7 @@ const LineChart: React.FC<LineChartProps> = ({
         options={chartOptions}
         series={chartSeries}
         type="line"
-        height={350}
+        height={270}
       />
     </div>
   );
@@ -86,8 +86,7 @@ const LineChartTest: React.FC = () => {
   ];
 
   return (
-    <div className="w-full h-full mt-24">
-      <h1>Monthly Performance</h1>
+    <div className="">
       <LineChart data={sampleData} show_category={true} />
     </div>
   );
