c90cb2add734523a2b7cbd33edae4b345a082fec
Smey <roemreaksmey7@gmail.com>
2024-11-06 13:47:56 +0700

    fix build on PM2 V2

--- apps/backend/auth-service/ecosystem.config.js
+++ apps/backend/auth-service/ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "auth-service", // 1. Name of the application
-      script: "server.js", // 2. The script file to execute
-      instances: 1, // 3. Number of instances to run
-      autorestart: true, // 4. Auto-restart the application if it crashes
-      watch: false, // 5. Do not watch for file changes
-      max_memory_restart: "1G", // 6. Restart the app if it exceeds 1 GB of memory usage
+      name: "auth-service",
+      script: "server.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
     },
   ],
 };
