f8c0479eac5b1e5b090db496508e9570ec84bb85
Easy Code <easycode002@gmail.com>
2024-11-12 09:16:10 +0700

    Added:

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -8,16 +8,16 @@ import {
 } from "react-router-dom";
 import Visualize from "./components/pages/Visualize";
 import Dataset from "./components/pages/Dataset";
-import Project from "./components/pages/Project";
 import Helps from "./components/pages/Helps";
 import AccountSettings from "./components/templates/AccountSettings";
+import DataFlow from "./components/templates/DataFlow";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Project />} />
+          <Route path="/" element={<DataFlow />} />
           <Route path="/visualize" element={<Visualize />} />
           <Route path="/dataset" element={<Dataset />} />
           <Route path="/helps" element={<Helps />} />
