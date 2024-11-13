eacbdaee4e87f1e7ee906d7e042999906c353e1d
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2024-11-13 12:37:18 +0700

    Latest Update

--- apps/client-app/app/resource/page.tsx
+++ apps/client-app/app/resource/page.tsx
@@ -7,7 +7,6 @@ import { HeroSection } from "./components/HeroSection";
 const ResourcePage = () => {
   return (
     <>
-      {/* Here */}
       <HeroSection />
       <DocumentationSection />
       <CTASection />
