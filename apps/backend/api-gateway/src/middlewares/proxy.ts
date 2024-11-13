f6dd9407e8ef5e0a023ac569bb7abeba9897d9a7
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2024-11-13 12:19:58 +0700

    Add apiGatway

---
+++ apps/backend/api-gateway/src/middlewares/proxy.ts
@@ -0,0 +1,161 @@
+import ROUTE_PATHS from "@/src/route-defs"
+import { logRequest } from "@/src/utils/logger"
+import express, { Response } from "express"
+import { ClientRequest, IncomingMessage } from "http"
+import { createProxyMiddleware, Options } from "http-proxy-middleware"
+import { gatewayLogger } from "@/src/server"
+import corsOptions from "@/src/middlewares/cors"
+
+interface ProxyConfig {
+  [context: string]: Options<IncomingMessage, Response>
+}
+
+const proxyConfigs: ProxyConfig = {
+  [ROUTE_PATHS.AUTH_SERVICE.path]: {
+    target: ROUTE_PATHS.AUTH_SERVICE.target,
+    pathRewrite: (path, _req) => {
+      return `${ROUTE_PATHS.AUTH_SERVICE.path}${path}`
+    },
+    on: {
+      proxyReq: (proxyReq: ClientRequest, _req: IncomingMessage, _res: Response) => {
+
+        // @ts-ignore
+        logRequest(gatewayLogger, proxyReq, {
+          protocol: proxyReq.protocol,
+          host: proxyReq.getHeader('host'),
+          path: proxyReq.path
+        });
+      },
+      proxyRes: (_proxyRes, _req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+      }
+    }
+  },
+  [ROUTE_PATHS.USER_SERVICE.path]: {
+    target: ROUTE_PATHS.USER_SERVICE.target,
+    pathRewrite: (path, _req) => `${ROUTE_PATHS.USER_SERVICE.path}${path}`,
+    on: {
+      proxyReq: (proxyReq: ClientRequest, _req: IncomingMessage, _res: Response) => {
+
+        // @ts-ignore
+        logRequest(gatewayLogger, proxyReq, {
+          protocol: proxyReq.protocol,
+          host: proxyReq.getHeader('host'),
+          path: proxyReq.path
+        });
+      },
+      proxyRes: (_proxyRes, _req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+      }
+    }
+  },
+  [ROUTE_PATHS.JOB_SERVICE.path]: {
+    target: ROUTE_PATHS.JOB_SERVICE.target,
+    pathRewrite: (path, _req) => `${ROUTE_PATHS.JOB_SERVICE.path}${path}`,
+    on: {
+      proxyReq: (proxyReq: ClientRequest, _req: IncomingMessage, _res: Response) => {
+
+        // @ts-ignore
+        logRequest(gatewayLogger, proxyReq, {
+          protocol: proxyReq.protocol,
+          host: proxyReq.getHeader('host'),
+          path: proxyReq.path
+        });
+      },
+      proxyRes: (_proxyRes, _req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+      }
+    }
+  },
+  [ROUTE_PATHS.NOTIFICATION_SERVICE.path]: {
+    target: ROUTE_PATHS.NOTIFICATION_SERVICE.target,
+    pathRewrite: (_path, _req) => {
+      return `${ROUTE_PATHS.NOTIFICATION_SERVICE.path}${_path}`
+    },
+    on: {
+      proxyReq: (proxyReq: ClientRequest, _req: IncomingMessage, _res: Response) => {
+        // @ts-ignore
+        logRequest(gatewayLogger, proxyReq, {
+          protocol: proxyReq.protocol,
+          host: proxyReq.getHeader('host'),
+          path: proxyReq.path
+        });
+      },
+      proxyRes: (_proxyRes, _req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+      }
+    }
+  },
+  [ROUTE_PATHS.CONVERSATION.path]: {
+    target: ROUTE_PATHS.CONVERSATION.target,
+    pathRewrite: (path, _req) => `${ROUTE_PATHS.CONVERSATION.path}${path}`,
+    on: {
+      proxyReq: (proxyReq: ClientRequest, _req: IncomingMessage, _res: Response) => {
+
+        // @ts-ignore
+        logRequest(gatewayLogger, proxyReq, {
+          protocol: proxyReq.protocol,
+          host: proxyReq.getHeader('host'),
+          path: proxyReq.path
+        });
+      },
+      proxyRes: (_proxyRes, _req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+      }
+    }
+  },
+  [ROUTE_PATHS.CHAT_SERVICE.path]: {
+    target: ROUTE_PATHS.CHAT_SERVICE.target,
+    ws: true,
+    on: {
+      error: (err, _req, _res) => {
+        console.log('error chat:::', err)
+      }
+    }
+  }
+}
+
+
+const applyProxy = (app: express.Application) => {
+  Object.keys(proxyConfigs).forEach((context: string) => {
+    // Apply the proxy middleware
+    if (context === ROUTE_PATHS.CHAT_SERVICE.path) {
+      app.use(createProxyMiddleware(proxyConfigs[context]));
+    } else {
+      app.use(context, createProxyMiddleware(proxyConfigs[context]))
+    }
+  })
+}
+
+
+export default applyProxy
