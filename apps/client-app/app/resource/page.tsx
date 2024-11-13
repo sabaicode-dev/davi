1fa7d67548ed36f154c4035e7963c5d6d56a5758
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2024-11-13 12:34:24 +0700

    Add Update

--- apps/client-app/app/resource/page.tsx
+++ apps/client-app/app/resource/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-
 import CTASection from "../home/components/CTASection";
 import DocumentationSection from "./components/DocumentationSection";
 import { HeroSection } from "./components/HeroSection";
 
 const ResourcePage = () => {
-
-
   return (
     <>
+      {/* Here */}
       <HeroSection />
       <DocumentationSection />
       <CTASection />
