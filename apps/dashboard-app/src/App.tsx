560bf0106058164b50a2d3fa73fc4941ce2de085
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2024-12-09 10:01:58 +0700

    Add envProduct

--- apps/dashboard-app/src/App.tsx
+++ apps/dashboard-app/src/App.tsx
@@ -15,6 +15,7 @@ import ProjectDetail from "@/src/components/molecules/project/ProjectDetail";
 import FinalScreen from "./components/molecules/steps/FinalScreen";
 import CleaningProject from "./components/molecules/steps/CleaningProject";
 import ProjectImport from "./components/molecules/steps/selectTable";
+import PrivateRoute from "./ProtectedRoute/PrivateRoute";
 const ProjectFlow = () => {
   return (
     <Routes>
@@ -66,7 +67,7 @@ export const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-        {/* <PrivateRoute> */}
+        <PrivateRoute>
         <Layout>
           <Routes>
             {routes.map((route) => (
@@ -78,7 +79,7 @@ export const App = () => {
             ))}
           </Routes>
         </Layout>
-        {/* </PrivateRoute> */}
+        </PrivateRoute>
       </BrowserRouter>
     </AuthProvider>
   );
