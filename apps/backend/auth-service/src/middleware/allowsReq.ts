c106b3143e646cb087e9b215cfa0457c80ed8a3a
Smey <roemreaksmey7@gmail.com>
2024-12-13 09:06:40 +0700

    allow corsOptions

--- apps/backend/auth-service/src/middleware/allowsReq.ts
+++ apps/backend/auth-service/src/middleware/allowsReq.ts
@@ -3,7 +3,10 @@ import cors from "cors";
 // Determine allowed origins based on environment
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? ["https://d3llp4uth9m31o.cloudfront.net"]
+    ? [
+        "https://d3llp4uth9m31o.cloudfront.net",
+        "https://dgm6c7j4yhtsc.cloudfront.net",
+      ]
     : ["http://localhost:3000", "http://localhost:8080"];
 
 // Export the CORS middleware configuration
